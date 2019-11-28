20f2b316e941272a946e704a0fef9dd5831264b4
Elke Burgers <elke.burgers@hotmail.com>
2019-11-28 10:01:22 -0300

    Criacao dos arquivos php no modelo MVC.

---
+++ models/Conexao.php
@@ -0,0 +1,9 @@
+<?php
+
+class Conexao{
+    private $host = "mysql:host=localhost;dbname=fakegram;port=3306";
+    private $user = "root";
+    private $pass = "";
+}
+
+?>
---
+++ views/publicacao.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="views/css/styles.css">
+</head>
+<body>
+    
+     <!-- header -->
+    <?php include "views/includes/header.php"; ?>
+
+    <!-- pagina de visualizacao de publicacoes -->
+    <main class="board">
+   
+        <div class="card mt-5">
+            <img id="publimg" src="views/img/teste.png" alt="Foto">
+            <div class="card-body">
+                <p class="card-text">
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate excepturi est impedit quidem. Voluptate quia temporibus id culpa perferendis tenetur voluptatem fugit, voluptas minima, rerum vel mollitia accusantium ipsum? Voluptatum.   
+                </p>
+            </div>
+        </div>
+      
+        <a class="float-button" href="/instagran/formulario-post">&#10010;</a>
+    </main>
+    
+<!-- links de javascript -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
